71609c0bcb505475f57dc162deb81d4a7e1a4cf1
Kavinprabu S M <100138892+kavin1711@users.noreply.github.com>
2023-09-06 15:40:57 +0530

    updated the code base

--- TLS_E2E_Automation/cypress.config.js
+++ TLS_E2E_Automation/cypress.config.js
@@ -1,17 +1,8 @@
 const { defineConfig } = require( 'cypress' );
 //const ReportGenerator = require('lighthouse/lighthouse-core/report/report-generator'); 
 //const ReportGenerator = require('lighthouse/report/generator/report-generator');
-const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
-const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
 
 
-
-async function setupNodeEvents(on, config) {
-  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
-await preprocessor.addCucumberPreprocessorPlugin(on, config);
-on("file:preprocessor", browserify.default(config));
-return config;
-}
 
 module.exports = defineConfig( {
 	projectId: 'prise6',
--- TLS_E2E_Automation/cypress/plugins/index.js
+++ TLS_E2E_Automation/cypress/plugins/index.js
@@ -1,43 +1,71 @@
 /// <reference types="cypress" />
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
 
-const cucumber = require('cypress-cucumber-preprocessor').default;
-const fs = require('fs');
-const { lighthouse, pa11y, prepareAudit } = require('cypress-audit');
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+
+const cucumber = require( 'cypress-cucumber-preprocessor' ).default;
+const fs = require( 'fs' );
+const { lighthouse, pa11y, prepareAudit } = require('cypress-audit'); 
+//const ReportGenerator = require('lighthouse/lighthouse-core/report/report-generator'); 
 const ReportGenerator = require('lighthouse/report/generator/report-generator');
 
-module.exports = (on) => {
-  on('file:preprocessor', cucumber());
 
-  on('before:browser:launch', (browser, launchOptions) => {
-    prepareAudit(launchOptions);
-    if (browser.name === 'chrome' && browser.isHeadless) {
-      launchOptions.args.push('--disable-gpu');
-      return launchOptions;
-    }
-  });
 
-  on('task', {
-    lighthouse: lighthouse((lighthouseReport) => {
-      fs.writeFileSync('cypress/screenshots/lhreport.html', ReportGenerator.generateReport(lighthouseReport.lhr, 'html'));
-    }),
-    pa11y: pa11y(),
-  });
+module.exports = ( on ) => {
+	on( 'file:preprocessor', cucumber() );
+	/*on('before:browser:launch', (browser = {}, launchOptions) => {
+		prepareAudit(launchOptions)
+	  })
+	
+	  on('task', {
+		lighthouse: lighthouse(), // calling the function is important
+	  })*/
+	  on('before:browser:launch', (browser, launchOptions) => {
 
-  on('task', {
-    log(message) {
-      console.log(message);
-      return null;
-    },
-    table(message) {
-      console.table(message);
-      return null;
-    },
-  });
+		prepareAudit(launchOptions); 
+		if (browser.name === 'chrome' && browser.isHeadless) {
+		  launchOptions.args.push('--disable-gpu');
+		  return launchOptions;
+		}
+	  });
+	
+	  on('task', {
+		lighthouse: lighthouse((lighthouseReport) => { 
+		  fs.writeFileSync('cypress/screenshots/lhreport.html', ReportGenerator.generateReport(lighthouseReport.lhr, 'html')); 
+		}),
+		pa11y: pa11y(), 
+	  });
+	  
+	on( 'task', {
+		log( message ) {
+			// eslint-disable-next-line no-console
+			console.log( message );
+			return null;
+		},
+		table( message ) {
+			// eslint-disable-next-line no-console
+			console.table( message );
+			return null;
+		},
 
-  on('after:run', (results) => {
-    if (results) {
-      fs.mkdirSync('cypress/.run', { recursive: true });
-      fs.writeFileSync('cypress/.run/results.json', JSON.stringify(results));
-    }
-  });
+	} );
+	on( 'after:run', ( results ) => {
+		if ( results ) {
+			fs.mkdirSync( 'cypress/.run', { recursive: true } );
+			fs.writeFile( 'cypress/.run/results.json', JSON.stringify( results ) );
+		}
+	} );
 };
