3ef16bcb6e36082052dcd5b202219a16ff83e406
cchiremath123 <chetana.hiremath@news.co.uk>
2023-10-18 19:05:07 +0530

    fixes for archivepage

--- TLS_E2E_Automation/cypress/support/tlshealthcheckcommands.js
+++ TLS_E2E_Automation/cypress/support/tlshealthcheckcommands.js
@@ -34,7 +34,7 @@ const collectionSlices="div[class*='slices']";
 const newsletterBlock="div[class='tls-newsletter']";
 
 //archive page elements
-const verifySearchFilter ='.tls-search-filter__input';
+const verifySearchFilter ='.tls-search-filter .tls-search-filter__input';
 const verifyDropdownForYears =".tls-date-filter__dropdown-btn";
 const selectYears ="[class*='tls-date-filter__content-item']";
 const verifySearchedAuthorPage="[class*='tls-search-core__hits']";
